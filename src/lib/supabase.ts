40fb123e9605928ad432c613556a6b705d90ff98
Sebastian Soza Madariaga <sebastian.soza.m@mail.pucv.cl>
2026-06-25 22:54:29 -0400

    fix: types path in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-import type { QuestionWithOptions } from './types'
+import type { QuestionWithOptions } from '../types'
 
 const url = import.meta.env.VITE_SUPABASE_URL as string
 const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string
